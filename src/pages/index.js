e1b4789df1a899f48678713313371e6dcab9cfc7
TDN <tedfish@gmail.com>
2021-04-06 12:10:14 -0700

    dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link, graphql } from 'gatsby'
 import Masonry from 'react-masonry-component'
 import Img from 'gatsby-image'
 import Layout from "../components/layout"
-
 const IndexPage = ({ data }) => (
   <Layout>
     <Masonry className="showcase">
@@ -14,7 +13,7 @@ const IndexPage = ({ data }) => (
               <Img fluid={work.coverImage.fluid} />
             </Link>
             <figcaption className="card__caption">
-              <h6 className="card__title">
+              <h6 className="card__title">****
                 <Link to={`/works/${work.slug}`}>{work.title}</Link>
               </h6>
               <div className="card__description">
@@ -27,9 +26,7 @@ const IndexPage = ({ data }) => (
     </Masonry>
   </Layout>
 )
-
 export default IndexPage
-
 export const query = graphql`
   query IndexQuery {
     allDatoCmsWork(sort: { fields: [position], order: ASC }) {
